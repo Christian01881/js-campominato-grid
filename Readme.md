cfa13ce16357d57332d82d4b690d11575379f5e9
Christian01881 <chrispamo01881@gmail.com>
2023-03-18 18:08:56 +0100

    add-html-css-js first && second milestone complete

---
+++ Readme.md
@@ -0,0 +1,3 @@
+Creare un evento sul submit per generre tutti i miei dati e iniziare il gioco
+Creare uno switch per cambiare il numero di caselle in bse alla difficoltà selezionata
+Creare un ciclo for per generare la scacchiera
---
+++ js/script.js
@@ -0,0 +1,61 @@
+/*
+Consegna
+L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+Ogni cella ha un numero progressivo, da 1 a 100.
+Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+Bonus
+Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
+- con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
+- con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
+- con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
+*/
+
+// Creo un Event submit che mi permette di selezionare il livello:
+
+const levelSelect = document.getElementById('LevelSelect');
+levelSelect.addEventListener('submit', play);
+
+function play(e) {
+    e.preventDefault();
+    const difficulty = document.getElementById('Difficulty').value;
+    console.log(difficulty);
+
+// Creo uno switch che cambia il numero di celle in base alla difficoltà:
+    let cellNumbers;
+    switch(difficulty){
+        case 'Easy':
+            cellNumbers = 100;
+            break;
+        case 'Medium':
+            cellNumbers = 81;
+            break;
+        case 'Hard':
+            cellNumbers = 49;
+            break;
+    };
+    console.log(cellNumbers);
+    let cellPerRow = Math.sqrt(cellNumbers);
+    console.log(cellPerRow);
+
+// Creo un ciclo for per generare la scacchiera:
+    const cellBox = document.querySelector('.CPM-box');
+    cellBox.innerHTML = '';
+    
+    for(let i = 1; i <= cellNumbers; i++){
+        let cell = document.createElement('div');
+        cell.classList.add('CPM-cell');
+        cell.style.width = `calc(100% / ${cellPerRow})`;
+        cell.style.height = cell.style.width;
+        cell.innerHTML = i;
+        cellBox.appendChild(cell);
+
+        cell.addEventListener('click', function(){
+            cell.classList.add('CPM-bg-green')
+            console.log(i)
+        })
+    };
+}
+
+// Creo un evento che cambia il colore delle celle al click:
+    
